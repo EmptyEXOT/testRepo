cf362e33f097a50c590c3de1713300994afb2981
EmptyEXOT <sadullaev.alim@mail.ru>
2022-06-29 08:50:47 +0300

    first version of group separator

--- script.js
+++ script.js
@@ -4,31 +4,37 @@ Promise = require('bluebird');
 let eachLine = Promise.promisify(lineReader.eachLine);
 const regExp = /\[.*\]/;
 let flag;
-let lines = [];
-let text = [];
 
-eachLine('test.txt', function(line) {
-    if (line.match(regExp)) {
-        switch (line) {
-            case '[lines]':
-                flag = 'lines'
+let controller = {
+    lines: [],
+    text: []
+}
+
+function parseConfig(controller) {
+    eachLine('test.txt', function(line) {
+        if (line.match(regExp)) {
+            switch (line) {
+                case '[lines]':
+                    flag = 'lines'
+                    break;
+                case '[text]':
+                    flag = 'text';
+                    break;
+                default: console.log('error');
+            }
+        } else switch (flag) {
+            case 'lines':
+                controller.lines.push(line);
                 break;
-            case '[text]':
-                flag = 'text';
+            case 'text':
+                controller.text.push(line);
                 break;
             default: console.log('error');
         }
-    } else switch (flag) {
-        case 'lines':
-            lines.push(line);
-            break;
-        case 'text':
-            text.push(line);
-            break;
-        default: console.log('error');
-    }
-}).then(()=>{
-    for (let i = 0; i<lines.length; i++) {
-        console.log(lines[i]);
-    }
-})
+    }).then(()=>{
+        console.log(controller.lines);
+    })
+}
+
+parseConfig(controller);
+
